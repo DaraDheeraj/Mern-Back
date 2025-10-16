99394a00ff1a19d6e68210f95b69ce4e34a22f77
DaraDheeraj <dheerajdara68@email.com>
2025-10-16 13:56:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express=require("express")
+const cors=require("cors")
+const app=express()
+const mongoose=require("mongoose")
+app.use(express.json())
+app.use(cors())
+mongoose.connect("mongodb://localhost:27017/counter")
+.then(()=>console.log("Mongo connected"))
+.catch((err)=>console.log("mongofailed",err))
+
+const todoSchema=new mongoose.Schema({
+    text:{
+        type:String,
+        required:true
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
+})
+
+
+const Todo=mongoose.model("Todo",todoSchema);
+
+app.delete('/delete/:id', async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.send("Deleted");
+})
+app.patch('/update/:id', async (req, res) => {
+    await Todo.findByIdAndUpdate(req.params.id,
+         { completed: req.body.completed });
+    res.send("Updated");
+    await todo.save();
+})
+
+app.post('/create', async (req, res) => {
+    const todo = await new Todo({
+        text: req.body.text,
+    })
+    await todo.save();
+    res.json(todo);
+})
+
+
+app.listen(7777, async () => {
+    console.log("7777 port");
+})
+
+app.get('/', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+})
